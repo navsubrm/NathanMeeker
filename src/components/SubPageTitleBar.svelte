e653dd45e2dc3b2468ecc9e1e758264f0cf63d54
Nate <navsubrm@gmail.com>
2021-10-21 01:06:14 -0700

    text scrolling

--- src/components/SubPageTitleBar.svelte
+++ src/components/SubPageTitleBar.svelte
@@ -3,7 +3,7 @@
 	export let colorVar: string;
 </script>
 
-<div class="title-bar">
+<div class="title-bar" style="background-color: {colorVar}">
 	<div class="header">
 		<h1>{title}</h1>
 	</div>
@@ -11,25 +11,25 @@
 
 <style>
 	.title-bar {
+		position: relative;
+		margin-top: 70px;
 		display: flex;
 		justify-content: center;
 		align-content: center;
-		height: 50vh;
-		background-color: var(--orange);
-		border: solid 1px black;
+		height: 20vh;
 	}
 	.header {
-		margin-top: 60px;
-		display: flex;
-		justify-content: center;
-		align-content: center;
+		margin-top: 0px;
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
 		width: fit-content;
-		width: 100%;
-		border: solid 1px black;
 	}
 	h1 {
-		font-size: 3em;
-		font-weight: bold;
+		font-size: 5em;
+		font-family: Roboto, Helvetica, sans-serif;
+		font-weight: 700;
 		color: white;
 		text-transform: uppercase;
 	}
--- src/routes/experience.svelte
+++ src/routes/experience.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
-	import { page } from '$app/stores';
 	import { fly, fade } from 'svelte/transition';
-	import SubTitleBar from '../components/SubTitleBar.svelte';
+	import SubPageTitleBar from '../components/SubPageTitleBar.svelte';
 </script>
 
 <svelte:head>
@@ -9,27 +8,31 @@
 </svelte:head>
 
 <div in:fly={{ y: -100, duration: 500, delay: 400 }} out:fade>
-	<SubTitleBar title={'experience'} colorVar={'--orange'} />
+	<SubPageTitleBar title={'experience'} colorVar={'rgb(238, 174, 37)'} />
 	<div class="container-border">
 		<div class="card-container">
-			<div class="card-sm">
-				<h1>General Experience</h1>
-				<div class="card-bg hidden">
-					<strong>24 years of professional experience in the US Navy:</strong>
-					<strong>Communications Security Manager (COMSEC) / Inspector 10+ years:</strong>
-					<ul>
-						<li>COMSEC(EKMS/KMI) Manager 5+ years.</li>
-						<li>COMSEC(EKMS/KMI) Inspector 10+ years.</li>
-						<li>
-							Navy Communications Security Management System <a
-								title="Navy COMSEC Management System webpage."
-								href="https://www.navifor.usff.navy.mil/ncms/">(NCMS)</a
-							> certified inspector since 2010.
-						</li>
-					</ul>
+			<div class="card-frame one">
+				<div class="card-sm">
+					<div class="perspective">
+						<h1 class="text">General Experience</h1>
+					</div>
+					<div class="card-bg hidden">
+						<strong>24 years of professional experience in the US Navy:</strong>
+						<strong>Communications Security Manager (COMSEC) / Inspector 10+ years:</strong>
+						<ul>
+							<li>COMSEC(EKMS/KMI) Manager 5+ years.</li>
+							<li>COMSEC(EKMS/KMI) Inspector 10+ years.</li>
+							<li>
+								Navy Communications Security Management System <a
+									title="Navy COMSEC Management System webpage."
+									href="https://www.navifor.usff.navy.mil/ncms/">(NCMS)</a
+								> certified inspector since 2010.
+							</li>
+						</ul>
+					</div>
 				</div>
 			</div>
-			<div class="card-sm">
+			<div class="card-sm two">
 				<h1>Cyber Security</h1>
 				<div class="card-bg hidden">
 					<strong>Information Systems Security Manager / Network administrator 10+ years:</strong>
@@ -41,10 +44,10 @@
 					</ul>
 				</div>
 			</div>
-			<div class="card-sm">
+			<div class="card-sm three">
 				<h1>Network Administration</h1>
 			</div>
-			<div class="card-sm">
+			<div class="card-sm four">
 				<h1>Web Development</h1>
 				<div class="card-bg hidden">
 					<ul>
@@ -55,7 +58,7 @@
 					</ul>
 				</div>
 			</div>
-			<div class="card-sm">
+			<div class="card-sm five">
 				<h1>Security Management</h1>
 				<div class="card-bg hidden">
 					<strong>Personnel Security Management 10+ years:</strong>
@@ -80,7 +83,7 @@
 					</ul>
 				</div>
 			</div>
-			<div class="card-sm">
+			<div class="card-sm six">
 				<h1>Education</h1>
 				<div class="card-bg hidden">
 					<strong
@@ -100,25 +103,52 @@
 </div>
 
 <style>
-	.title {
-		text-align: center;
-		text-transform: uppercase;
-		font-size: 3em;
+	.card-frame {
+		width: 400px;
+		height: 400px;
+		margin: 200px;
+		perspective: 400px;
+		perspective-origin: 50% 50%;
+		position: relative;
+	}
+
+	.perspective {
+		transform-style: preserve-3d;
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate3d(-50%, -50%, 0);
+	}
+
+	.text {
+		font-size: 1.5em;
+		-webkit-backface-visibility: hidden;
+		-moz-backface-visibility: hidden;
+		backface-visibility: hidden;
+		animation: rotate 6s linear infinite;
+	}
+	.text:hover {
+		transform: rotateY(0deg) translateZ(200px);
+		animation-play-state: paused;
+		-o-animation-play-state: paused;
+		-webkit-animation-play-state: paused;
+		-moz-animation-play-state: paused;
 	}
 
 	.card-sm {
 		position: relative;
 		width: 300px;
 		height: 300px;
-		padding: 40px;
+		padding: 30px;
 		display: flex;
 		justify-content: center;
 		align-items: center;
 		text-align: center;
 		font-size: 1.3em;
 		border-radius: 50%;
-		box-shadow: inset 0px 5px 280px var(--orange);
+		box-shadow: inset 0px 5px 380px var(--orange), inset 0px 5px 60px rgb(10, 10, 10);
 		cursor: pointer;
+		z-index: 2;
 	}
 	.card-sm::after {
 		content: '';
@@ -138,16 +168,48 @@
 
 	.card-container {
 		display: grid;
-		grid-template-columns: repeat(3, 1fr);
-		grid-template-rows: auto;
+		grid-template-areas:
+			'. one two .'
+			'six . . three'
+			'. five four .';
+		grid-template-columns: repeat(4, 1fr);
+		grid-template-rows: repeat(3, fit-content);
 		align-items: center;
 		justify-items: center;
-		align-items: center;
-		grid-gap: 5em;
-		padding: 5em;
+		width: fit-content;
+		margin: 20px auto 60px auto;
+		grid-gap: 3em;
+	}
+
+	.one {
+		grid-area: one;
+		grid-column: 2/3;
+	}
+	.two {
+		grid-area: two;
+		grid-column: 3/4;
+	}
+	.three {
+		grid-area: three;
+		grid-column: 3/5;
+	}
+	.four {
+		grid-area: four;
+		grid-column: 3/4;
+	}
+	.five {
+		grid-area: five;
+		grid-column: 2/3;
+	}
+	.six {
+		grid-area: six;
+		grid-column: 1/3;
 	}
 	.center-text {
 		text-align: center;
+		padding: 15px;
+		font-weight: 400;
+		font-size: 1.5em;
 	}
 
 	ul,
@@ -156,5 +218,14 @@
 		list-style: square;
 		margin-left: 20px;
 		text-decoration: none;
+	}
+
+	@keyframes rotate {
+		from {
+			transform: rotateY(0deg) translateZ(200px);
+		}
+		to {
+			transform: rotateY(360deg) translateZ(200px);
+		}
 	}
 </style>
